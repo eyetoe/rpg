afc1d1daec8f1b83b79c93cd4975800bed370463
John Curry <johnny@simpleenergy.com>
2014-01-12 11:06:57 -0700

    setting up new better getch for class calls

--- lib/getch2.py
+++ lib/getch2.py
@@ -4,34 +4,53 @@ import sys
 import termios
 import fcntl
 
-def myGetch():
-    fd = sys.stdin.fileno()
+class _Getch:
+    def __init__(self):
+        try:
+            self.impl = _myGetch()
+        except ImportError:
+            print 'exiting, cant call _myGetch()'
 
-    oldterm = termios.tcgetattr(fd)
-    newattr = termios.tcgetattr(fd)
-    newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
-    termios.tcsetattr(fd, termios.TCSANOW, newattr)
+    def __call_(self): return self.impl()
 
-    oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
-    fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
+class _myGetch:
+    def __init__(self):
+        import tty, sys
 
-    try:        
-        while 1:            
-            try:
-                c = sys.stdin.read(1)
-                break
-            except IOError: pass
-    finally:
-        termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
-        fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
-    return c
+    def __call__(self):
+#def _myGetch():
+        import os
+        import sys    
+        import termios
+        import fcntl
+        fd = sys.stdin.fileno()
+
+        oldterm = termios.tcgetattr(fd)
+        newattr = termios.tcgetattr(fd)
+        newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+        termios.tcsetattr(fd, termios.TCSANOW, newattr)
+    
+        oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
+        fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
+    
+        try:        
+            while 1:            
+                try:
+                    c = sys.stdin.read(1)
+                    break
+                except IOError: pass
+        finally:
+            termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+            fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
+        return c
 
 if __name__ == '__main__': # a little test
    print 'Press a key'
-   inkey = myGetch()
+   inkey = _myGetch()
    import sys
    for i in xrange(sys.maxint):
-      #k=inkey()
+      k=inkey()
       if inkey <>'':break
-   print 'you pressed', inkey
+   print 'you pressed', k
+   #print 'you pressed', inkey
 
